1710eb8311b7db4c8bda3cf2a7f892c18d769806
Luka <luka.luka.luka93@gmail.com>
2025-03-12 14:17:08 -0700

    added logo and dynamic hover effect on it

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,16 @@
+import {useState} from 'react';
+
 import { FaBarsStaggered } from "react-icons/fa6";
+import { TfiHome } from "react-icons/tfi";
 
 export default function NavBar(){
+    const [isOpen, setIsOpen] = useState(false);
+
+    function toggleMenu() {
+        setIsOpen(!isOpen);
+    }
+
+
     return (
         <>
             <header>
@@ -9,14 +19,14 @@ export default function NavBar(){
                         <div className='logo'>
                             <h2>logo image</h2>
                         </div>
-                        <ul className="nav-link">
+                        <ul className={isOpen ? 'nav-link active' : 'nav-link'}>
                             <li><a href='#' className='active'>Home</a></li>
                             <li><a href='#'>About</a></li>
                             <li><a href='#'>Services</a></li>
                             <li><a href='#'>Portfolio</a></li>
                             <li><a href='#'>Contact Me</a></li>
                         </ul>
-                        <div className='sideBar-icon'>
+                        <div className='sideBar-icon' onClick={toggleMenu}>
                            <FaBarsStaggered color='white'/> 
                         </div>
                     </nav>
@@ -25,7 +35,7 @@ export default function NavBar(){
             <section>
                 <div className='navBar-container'>
                     <div className='responsive-navBar-content'>
-                        <h2>EcoRise</h2>
+                        <h2>Eco<span className='house-icon'>{<TfiHome size='7rem'/>}</span>Rise</h2>
                         <h3>construction</h3>
                     </div>
 
