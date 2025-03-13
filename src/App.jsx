3d550daf661be0d6a9e932049446a232d80b46b6
Luka <luka.luka.luka93@gmail.com>
2025-03-13 14:08:04 -0700

    added services

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import video from './assets/imgs/video-const.mp4';
 import { TfiHome } from "react-icons/tfi";
 
+// images:
+import landscape from './assets/imgs/landscaping.jpeg';
+import hardscaping from './assets/imgs/hardscaping.jpeg';
+import kitchen from './assets/imgs/kitchen1.jpeg';
+import bathroom from './assets/imgs/bathroom.jpg';
+import paint from './assets/imgs/paint.jpg';
+
 
 import NavBar from './components/NavBar/NavBar.jsx'
 import About from './components/About/About.jsx';
 import ImageSlider from './components/Portfolio-image-slider/Portfolio-image-slider.jsx';
+import WhatWeDo from './components/WhatWeDo/WhatWeDo.jsx';
 
 function App() {
   return (
@@ -22,6 +30,15 @@ function App() {
           <div className='information-wrapper'>
             <h4>Our Projects</h4>
             <ImageSlider />
+            <div className='what-we-do-wrapper'>
+              <h1>Our Main Services</h1>
+              <WhatWeDo img={landscape} alt='landscaping' service='Landscaping'/>
+              <WhatWeDo img={hardscaping} alt='hardscaping' service='Hardscaping'/>
+              <WhatWeDo img={kitchen} alt='kitchen' service='Kitchen'/>
+              <WhatWeDo img={bathroom} alt='Bathroom' service='Bathroom'/>
+              <WhatWeDo img={paint} alt='paint' service='Interior / Exterior Paint'/>
+
+            </div>
             <About />
           </div>
         </main>
---
+++ src/components/WhatWeDo/WhatWeDo.jsx
@@ -0,0 +1,17 @@
+import './WhatWeDo.css'
+
+export default function WhatWeDo({img, service, alt}){
+
+    return (
+        <>
+            <div className='what-we-do-section'>
+                <img src={img} alt={alt} />
+                <h5 className='service'>{service}</h5>
+                <button>More Info</button>
+            </div>
+            <svg className='services-line' width="100%" height="10">
+                <line x1="0" y1="5" x2="100%" y2="5" stroke="white" stroke-width="2"/>
+            </svg>
+        </>
+    )
+}
