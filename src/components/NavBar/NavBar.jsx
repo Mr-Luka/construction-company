950cbde5cd8c560d307ca2719383494f617acec6
Luka <luka.luka.luka93@gmail.com>
2025-03-16 17:59:54 -0700

    fixed nav link as well

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,37 +1,50 @@
-import {useState} from 'react';
-
-import { FaBarsStaggered } from "react-icons/fa6";
-import { TfiHome } from "react-icons/tfi";
+import { useState } from 'react';
+import { FaBarsStaggered } from 'react-icons/fa6';
+import { TfiHome } from 'react-icons/tfi';
 import './NavBar.css';
 
-export default function NavBar(){
-    const [isOpen, setIsOpen] = useState(false);
+export default function NavBar() {
+    const [isOpen, setIsOpen] = useState(false); // Initialize isOpen to false
 
     function toggleMenu() {
-        setIsOpen(!isOpen);
+        setIsOpen(isOpen=> !isOpen);
     }
 
     return (
         <>
             <header>
-                <div className='navBar-container'>
+                <div className="navBar-container">
                     <nav>
-                        <div className='logo'>
-                            <h2><TfiHome size="1.5rem"/></h2>
+                        <div className="logo">
+                            <h2>
+                                <TfiHome size="1.5rem" />
+                            </h2>
                         </div>
                         <ul className={isOpen ? 'nav-link active' : 'nav-link'}>
-                            <li><a href='#' className='active'>Home</a></li>
-                            <li><a href='#'>About</a></li>
-                            <li><a href='#'>Services</a></li>
-                            <li><a href='#'>Portfolio</a></li>
-                            <li><a href='#'>Contact Me</a></li>
+                            <li>
+                                <a href="#" className="active">
+                                    Home
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">About</a>
+                            </li>
+                            <li>
+                                <a href="#">Services</a>
+                            </li>
+                            <li>
+                                <a href="#">Portfolio</a>
+                            </li>
+                            <li>
+                                <a href="#">Contact Me</a>
+                            </li>
                         </ul>
-                        <div className='sideBar-icon' onClick={toggleMenu}>
-                           <FaBarsStaggered color='white'/> 
+                        <div className="sideBar-icon" onClick={toggleMenu}>
+                            <FaBarsStaggered color="white" />
                         </div>
                     </nav>
                 </div>
             </header>
         </>
-    )
+    );
 }
