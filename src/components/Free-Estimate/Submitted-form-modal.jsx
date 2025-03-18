aedce84cf045ef983a46d0025e7a46e6c5cfefda
Luka <luka.luka.luka93@gmail.com>
2025-03-18 13:30:13 -0700

    finished css for modal

--- src/components/Free-Estimate/Submitted-form-modal.jsx
+++ src/components/Free-Estimate/Submitted-form-modal.jsx
@@ -1,13 +1,19 @@
 import {useState, useRef} from 'react';
 
-export default function (){
+export default function ({name}){
 
 
 
 
     return (
         <div className="submitted-form-modal-wrapper">
-            <h1>here i am</h1>
+            <form method="dialog">
+                <button>X</button>
+            </form>
+            <h3>{name}Bojana Vujosevic</h3>
+            <h1>Your form has been submitted</h1>
+            <p>Thank you for taking the time to fill out our form. We will review your
+            submission and get back to you as soon as possible.</p>
         </div>
     )
 }
