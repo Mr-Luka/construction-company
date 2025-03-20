a8c030ee963334aee5f26c32ff29cefda9fccf1c
Luka <luka.luka.luka93@gmail.com>
2025-03-20 13:27:05 -0700

    finished css for the portfolio component

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,17 +1,21 @@
 import './Portfolio.css';
 import ProjectPortfolio from './Project-portfolio.jsx';
 
+import kitchen from '../../assets/imgs/kitchen1.jpeg'
+
 export default function Portfolio(){
 
     return(
         <div className="portfolio-wrapper">
-            <h1>Portfolio</h1>
-            <ProjectPortfolio project='Roofs' description='Our roofs are strong' />
-            <ProjectPortfolio project='Windows' description='Our windows are strong' />
-            <ProjectPortfolio project='Paint' description='Our paints are strong' />
-            <ProjectPortfolio project='Backyard' description='Our backyards are strong' />
-            <ProjectPortfolio project='Kitchen' description='Our kitchens are strong' />
-            <ProjectPortfolio project='Bathroom' description='Our bathrooms are strong' />
+            <h1>Our Projects</h1>
+            <div className='our-projects'>
+                <ProjectPortfolio project='Roofs' img={kitchen} description='Our roofs are strong' />
+                <ProjectPortfolio project='Windows' img={kitchen} description='Our windows are strong' />
+                <ProjectPortfolio project='Paint' img={kitchen} description='Our paints are strong' />
+                <ProjectPortfolio project='Backyard' img={kitchen} description='Our backyards are strong' />
+                <ProjectPortfolio project='Kitchen' img={kitchen} description='Our kitchens are strong' />
+                <ProjectPortfolio project='Bathroom' img={kitchen} description='Our bathrooms are strong' />
+            </div>
         </div>
     )
 }
