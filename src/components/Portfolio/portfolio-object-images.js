4c37f95313bfbdb5c942de7ba573db79a89f88b6
Luka <luka.luka.luka93@gmail.com>
2025-03-25 11:02:59 -0700

    imported images and made them into an object so i can loop them

---
+++ src/components/Portfolio/portfolio-object-images.js
@@ -0,0 +1,122 @@
+import roof1 from '../../assets/imgs/Portfolio/roof/roof-1.jpg';
+import roof2 from '../../assets/imgs/Portfolio/roof/roof-2.jpg';
+import roof3 from '../../assets/imgs/Portfolio/roof/roof-3.jpg';
+import roof4 from '../../assets/imgs/Portfolio/roof/roof-4.jpg';
+import roof5 from '../../assets/imgs/Portfolio/roof/roof-5.jpg';
+import roof6 from '../../assets/imgs/Portfolio/roof/roof-6.jpg';
+import roof7 from '../../assets/imgs/Portfolio/roof/roof-7.jpg';
+import roof8 from '../../assets/imgs/Portfolio/roof/roof-8.jpg';
+
+import window1 from '../../assets/imgs/Portfolio/windows/window-1.jpg';
+import window2 from '../../assets/imgs/Portfolio/windows/window-2.jpg';
+import window3 from '../../assets/imgs/Portfolio/windows/window-3.jpg';
+import window4 from '../../assets/imgs/Portfolio/windows/window-4.jpg';
+import window5 from '../../assets/imgs/Portfolio/windows/window-5.jpg';
+import window6 from '../../assets/imgs/Portfolio/windows/window-6.jpg';
+import window7 from '../../assets/imgs/Portfolio/windows/window-7.jpg';
+import window8 from '../../assets/imgs/Portfolio/windows/window-8.jpg';
+
+import paint1 from '../../assets/imgs/Portfolio/paint/paint-1.jpg';
+import paint2 from '../../assets/imgs/Portfolio/paint/paint-2.jpg';
+import paint3 from '../../assets/imgs/Portfolio/paint/paint-3.jpg';
+import paint4 from '../../assets/imgs/Portfolio/paint/paint-4.jpg';
+import paint5 from '../../assets/imgs/Portfolio/paint/paint-5.jpg';
+import paint6 from '../../assets/imgs/Portfolio/paint/paint-6.jpg';
+import paint7 from '../../assets/imgs/Portfolio/paint/paint-7.jpg';
+import paint8 from '../../assets/imgs/Portfolio/paint/paint-8.jpg';
+
+import backyard1 from '../../assets/imgs/Portfolio/backyard/backyard-1.jpg';
+import backyard2 from '../../assets/imgs/Portfolio/backyard/backyard-2.jpg';
+import backyard3 from '../../assets/imgs/Portfolio/backyard/backyard-3.jpg';
+import backyard4 from '../../assets/imgs/Portfolio/backyard/backyard-4.jpg';
+import backyard5 from '../../assets/imgs/Portfolio/backyard/backyard-5.jpg';
+import backyard6 from '../../assets/imgs/Portfolio/backyard/backyard-6.jpg';
+import backyard7 from '../../assets/imgs/Portfolio/backyard/backyard-7.jpg';
+import backyard8 from '../../assets/imgs/Portfolio/backyard/backyard-8.jpg';
+
+import kitchen1 from '../../assets/imgs/Portfolio/kitchen/kitchen-1.jpg';
+import kitchen2 from '../../assets/imgs/Portfolio/kitchen/kitchen-2.jpg';
+import kitchen3 from '../../assets/imgs/Portfolio/kitchen/kitchen-3.jpg';
+import kitchen4 from '../../assets/imgs/Portfolio/kitchen/kitchen-4.jpg';
+import kitchen5 from '../../assets/imgs/Portfolio/kitchen/kitchen-5.jpg';
+import kitchen6 from '../../assets/imgs/Portfolio/kitchen/kitchen-6.jpg';
+import kitchen7 from '../../assets/imgs/Portfolio/kitchen/kitchen-7.jpg';
+import kitchen8 from '../../assets/imgs/Portfolio/kitchen/kitchen-8.jpg';
+
+import bathroom1 from '../../assets/imgs/Portfolio/bathroom/bathroom-1.jpg';
+import bathroom2 from '../../assets/imgs/Portfolio/bathroom/bathroom-2.jpg';
+import bathroom3 from '../../assets/imgs/Portfolio/bathroom/bathroom-3.jpg';
+import bathroom4 from '../../assets/imgs/Portfolio/bathroom/bathroom-4.jpg';
+import bathroom5 from '../../assets/imgs/Portfolio/bathroom/bathroom-5.jpg';
+import bathroom6 from '../../assets/imgs/Portfolio/bathroom/bathroom-6.jpg';
+import bathroom7 from '../../assets/imgs/Portfolio/bathroom/bathroom-7.jpg';
+import bathroom8 from '../../assets/imgs/Portfolio/bathroom/bathroom-8.jpg';
+
+export const roofs = [
+    {url: roof1, title: 'house-roof1'},
+    {url: roof2, title: 'house-roof2'},
+    {url: roof3, title: 'house-roof3'},
+    {url: roof4, title: 'house-roof4'},
+    {url: roof5, title: 'house-roof5'},
+    {url: roof6, title: 'house-roof6'},
+    {url: roof7, title: 'house-roof7'},
+    {url: roof8, title: 'house-roof8'},
+]
+
+export const windows = [
+    {url: window1, title: 'windows1'},
+    {url: window2, title: 'windows2'},
+    {url: window3, title: 'windows3'},
+    {url: window4, title: 'windows4'},
+    {url: window5, title: 'windows5'},
+    {url: window6, title: 'windows6'},
+    {url: window7, title: 'windows7'},
+    {url: window8, title: 'windows8'},
+]
+
+export const paint = [
+    {url: paint1, title: 'paint1'},
+    {url: paint2, title: 'paint2'},
+    {url: paint3, title: 'paint3'},
+    {url: paint4, title: 'paint4'},
+    {url: paint5, title: 'paint5'},
+    {url: paint6, title: 'paint6'},
+    {url: paint7, title: 'paint7'},
+    {url: paint8, title: 'paint8'},
+]
+
+export const backyard = [
+    {url: backyard1, title: 'backyard1'},
+    {url: backyard2, title: 'backyard2'},
+    {url: backyard3, title: 'backyard3'},
+    {url: backyard4, title: 'backyard4'},
+    {url: backyard5, title: 'backyard5'},
+    {url: backyard6, title: 'backyard6'},
+    {url: backyard7, title: 'backyard7'},
+    {url: backyard8, title: 'backyard8'},
+]
+
+
+export const kitchen = [
+    {url: kitchen1, title: 'kitchen1'},
+    {url: kitchen2, title: 'kitchen2'},
+    {url: kitchen3, title: 'kitchen3'},
+    {url: kitchen4, title: 'kitchen4'},
+    {url: kitchen5, title: 'kitchen5'},
+    {url: kitchen6, title: 'kitchen6'},
+    {url: kitchen7, title: 'kitchen7'},
+    {url: kitchen8, title: 'kitchen8'},
+]
+
+export const bathrooms = [
+    {url: bathroom1, title: 'bathroom1'},
+    {url: bathroom2, title: 'bathroom2'},
+    {url: bathroom3, title: 'bathroom3'},
+    {url: bathroom4, title: 'bathroom4'},
+    {url: bathroom5, title: 'bathroom5'},
+    {url: bathroom6, title: 'bathroom6'},
+    {url: bathroom7, title: 'bathroom7'},
+    {url: bathroom8, title: 'bathroom8'},
+]
+
+
