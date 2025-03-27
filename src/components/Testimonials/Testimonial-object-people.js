3e671412496cb81b03dfe8f5043294914a1b0bf8
Luka <luka.luka.luka93@gmail.com>
2025-03-27 13:22:10 -0700

    added testimonial people object

---
+++ src/components/Testimonials/Testimonial-object-people.js
@@ -0,0 +1,36 @@
+import testiVideo1 from '../../assets/imgs/TestimonialsData/video/testi1.mp4';
+import testiVideo2 from '../../assets/imgs/TestimonialsData/video/testi2.mp4';
+import testiVideo3 from '../../assets/imgs/TestimonialsData/video/testi3.mp4';
+import testiVideo4 from '../../assets/imgs/TestimonialsData/video/testi4.mp4';
+
+import testiFace1 from '../../assets/imgs/TestimonialsData/faces/face1.jpg';
+import testiFace2 from '../../assets/imgs/TestimonialsData/faces/face2.jpg';
+import testiFace3 from '../../assets/imgs/TestimonialsData/faces/face3.jpg';
+import testiFace4 from '../../assets/imgs/TestimonialsData/faces/face4.jpg';
+import testiFace5 from '../../assets/imgs/TestimonialsData/faces/face5.jpg';
+import testiFace6 from '../../assets/imgs/TestimonialsData/faces/face6.jpg';
+import testiFace7 from '../../assets/imgs/TestimonialsData/faces/face7.jpg';
+import testiFace8 from '../../assets/imgs/TestimonialsData/faces/face8.jpg';
+import testiFace9 from '../../assets/imgs/TestimonialsData/faces/face9.jpg';
+import testiFace10 from '../../assets/imgs/TestimonialsData/faces/face10.jpg';
+
+
+export const testimonialVideos = [
+    {video: testiVideo1},
+    {video: testiVideo2},
+    {video: testiVideo3},
+    {video: testiVideo4},
+]
+
+export const testimonialData = [
+    {image: testiFace1, name: "Robert", profession: "Teacher",  text: 'Fast, reliable, and the quality is top-notch. Highly recommend their skilled team.'},
+    {image: testiFace2, name: "John", profession: "Farmer", text: "From the initial consultation to the final walkthrough, their team was incredibly professional and attentive. They listened to our needs and provided expert advice, ensuring the project was completed to our exact specifications. The quality of their work is outstanding, and we couldn't be happier with the results."},
+    {image: testiFace3, name: "Marun", profession: "Construction worker", text: "Quality work, fair pricing, and professional service. They exceeded our expectations."},
+    {image: testiFace4, name: "Mara", profession: "Retired teacher", text: "We hired them for a full home renovation, and they exceeded our expectations in every way. The project was completed on time and within budget, with exceptional attention to detail. Their communication was excellent throughout, keeping us informed every step of the way. We highly recommend their services."},
+    {image: testiFace5, name: "Sara", profession: "Accountant", text: "Their crew was punctual, courteous, and highly skilled. They maintained a clean and organized worksite, minimizing disruption to our daily life. The quality of their craftsmanship is evident in every aspect of the finished project. We are extremely satisfied with their work and would gladly hire them again."},
+    {image: testiFace6, name: "David", profession: "Security", text: "Their crew was reliable and professional. They arrived on time each day and worked diligently to complete the project. They maintained a clean and organized worksite, and their communication was excellent. The quality of their workmanship is outstanding, and we are extremely satisfied with the results."},
+    {image: testiFace7, name: "Ryan", profession: "Police Officer", text: "They transformed our space! Efficient, courteous, and highly skilled workers."},
+    {image: testiFace8, name: "Michael", profession: "FireFighter", text: "Great communication and timely completion. Their expertise is truly impressive."},
+    {image: testiFace9, name: "Jessica", profession: "SoftwareDeveloper", text: "We were impressed by their attention to detail and commitment to customer satisfaction. They provided a thorough estimate and kept us informed of any changes or updates. The project was completed on time, and the results are beautiful. We highly recommend their services to anyone seeking quality construction work."},
+    {image: testiFace10, name: "William", profession: "Fitness Trainer", text: "Their team was incredibly helpful and supportive throughout our home addition project. They offered creative solutions and provided expert guidance, ensuring the project met our needs and exceeded our expectations. The quality of their work is exceptional, and we are thrilled with our new space."},
+];
